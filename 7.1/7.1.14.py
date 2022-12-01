f81d02f491297afd2828cef47dd34af1bd0be94a
Jakub Olszewski <j.olszewski05@gmail.com>
2022-12-01 19:36:17 +0100

    1

---
+++ 7.1/7.1.14.py
@@ -0,0 +1,6 @@
+a = int(input())
+
+for x in range(a):
+    for y in range(a):
+        print("K",end="")
+    print()
---
+++ 7.1/7.1.19.py
@@ -0,0 +1,6 @@
+n = int(input())
+num = 0
+c = 0
+while num // 3 < n:
+    print(num, end=", ")
+    num += 3
